c5399083952a60f7eb81bb268e933cc3b8066804
mallowlabs <mallowlabs@gmail.com>
2011-05-18 09:22:03 +0900

    Added: autofocus to textbox

--- init.rb
+++ init.rb
@@ -2,13 +2,13 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_incr_code_search do
   name 'Redmine Incr Code Search plugin'
-  author 'Author name'
+  author 'suer & mallowlabs in codefirst.org'
   description 'Incremental Code Search plugin'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
+  url 'https://github.com/codefirst/redmine_incr_code_search'
   author_url 'http://d.hatena.ne.jp/suer'
 
   permission :hello, {:incr_code_search => [:search]}, :public => true
   menu :project_menu, :incr_code_search, {:controller => 'incr_code_search', :action => 'search'},
-  :caption => 'incr search', :param => :project_id
+  :caption => :menu_caption, :param => :project_id
 end
