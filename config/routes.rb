00c097976c0a5850565c86249392ff37e744858e
suer <suetsugu.r@gmail.com>
2012-05-04 01:31:19 +0900

    for redmine2.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,1 @@
-ActionController::Routing::Routes.draw do |map|
-  map.connect 'incr_code_search/:project_id/:action.:format', :controller => 'incr_code_search'
-end
+match 'incr_code_search/:action(.:format)', :controller => 'incr_code_search'
